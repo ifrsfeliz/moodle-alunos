acb0794230fc2ebc3e96bc6c3e7f9092d4e781f6
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2015-03-16 15:48:38 -0300

    Filtering users that are assigned to any category, not any user, otherwise admin, moderators, professors will be listed also

--- app/models/moodle_user.rb
+++ app/models/moodle_user.rb
@@ -2,7 +2,16 @@ class MoodleUser < ActiveRecord::Base
   establish_connection :moodle
 
   def self.all
-    connection.select_all('SELECT * FROM mdl_user u ORDER BY u.firstname ASC')
+    connection.select_all("
+      SELECT u.username, u.firstname, u.lastname, u.email, u.city, u.country
+      FROM mdl_role_assignments AS ra
+      JOIN mdl_context AS context ON context.id = ra.contextid AND context.contextlevel = 50
+      JOIN mdl_course AS c ON c.id = context.instanceid
+      JOIN mdl_course_categories AS cc ON cc.id = c.category
+      JOIN mdl_user AS u ON u.id = ra.userid
+      WHERE ra.roleid = 5 AND cc.path LIKE '/%'
+      GROUP BY u.email
+      ORDER BY u.firstname")
   end
 
   def self.all_categories
